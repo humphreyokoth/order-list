d5aac24c135586cbde3d7519d97f203bdaa80517
humphreyokoth <humpasp@gmail.com>
2023-07-03 12:00:44 +0300

    Feat:table

---
+++ src/components/Table.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+new Vue({})
+</script>
+
+<template>
+    <table class="table table-boardered table-stripped">
+        <thead>
+            <tr></tr>
+        </thead>
+    </table>
+</template>
---
+++ src/views/TableView.vue
@@ -0,0 +1,34 @@
+
+<template>
+<div id="app">
+<div class="text-uppercase text-bold">id selected:{{selected}}</div>
+<table class="table table-striped table-hover">
+<thead>
+    <tr>
+        <th><label class="form-checkbox">
+            <input  type="checkbox" v-model="selectAll" @click="select">
+            <i class="form-icon"></i>
+        </label></th>
+        <th>id</th>
+        <th>name</th>
+        <th>email</th>
+        
+    </tr>
+</thead>
+<tbody>
+    <tr v-for="i in items">
+    <td>
+        <label class="form-checkbox">
+            <input type="checkbox" :value="i.id" v-model="selected">
+            <i class="form-icon"></i>
+        </label>
+    </td>
+    <td>{{ i.id }}</td>
+    <td>{{ i.name }}</td>
+    <td>{{ i.email }}</td>
+    </tr>
+</tbody>
+</table>
+</div>
+
+</template>
