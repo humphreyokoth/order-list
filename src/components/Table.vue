7356c8552b195bcd6c58442c0f5e3f109fee22b1
humphreyokoth <humpasp@gmail.com>
2023-07-03 12:14:47 +0300

    Feat:table checkbox

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,33 @@
-<script setup lang="ts">
-new Vue({})
+<script setup lang="">
+new Vue({
+   el:"#app",
+   data:()=>({
+    items: [
+        {
+            id:'id1',
+            name:'john',
+            email:'email@example.com'
+        },
+        {
+            id:'id2',
+            name:'john',
+            email:'email2@example.com'  
+        }
+    ],selected:[],
+    selectAll:false
+   }),
+   methods :{
+    select(){
+        this.selected =[];
+        if (!this.selectAll) {
+           for (let i in this.items) {          
+            this.selected.push(this.items[i].id);              
+            
+           } 
+        }
+    }
+   }
+})
 </script>
 
 <template>
